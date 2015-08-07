6feb77c47b47a38d8bc51915469f96c364bfa541
Leonel Perea <iticleonel.len@gmail.com>
2015-08-06 20:49:58 -0500

    show active options

--- js/functionality.js
+++ js/functionality.js
@@ -1,1 +1,15 @@
 $(document).foundation();
+
+var $j = jQuery.noConflict();
+
+$j(document).ready(function(){
+
+  $j(document.body).on('click', '.pricing-table', function(){
+    console.log($j(this).data('id'));
+    if( !$j(this).hasClass("active") )
+      $j(this).addClass("active");
+    else
+      $j(this).removeClass("active");
+  });
+
+});
